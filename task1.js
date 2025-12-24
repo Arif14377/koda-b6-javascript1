38d5aef8d12a3b478c9e42e2254aca778ea16f08
Arif14377 <52228176+Arif14377@users.noreply.github.com>
2025-12-24 11:40:35 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -6,7 +6,7 @@ let jariJariLingkaran = 14
 const pi = 3.14
 
 let luasLingkaran = pi * jariJariLingkaran * jariJariLingkaran
-console.log("Hasil luas lingkaran adalah: ", luasLingkaran)
+console.log(luasLingkaran)
 
 let kelilingLingkaran = 2 * pi * jariJariLingkaran
-console.log("Hasil keliling lingkaran adalah: ", kelilingLingkaran)
+console.log(kelilingLingkaran)
