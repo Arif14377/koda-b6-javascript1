92f523ca16164a9b7cf1f6d858fe327d207a39bb
Arif14377 <52228176+Arif14377@users.noreply.github.com>
2025-12-24 10:47:42 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,12 @@
+// buat program hitung luas dan keliling lingkaran
+// menggunakan variabel dan data
+// koda-b6-javascript1
+
+let jariJariLingkaran = 14
+const pi = 3.14
+
+let luasLingkaran = pi * jariJariLingkaran * jariJariLingkaran
+console.log("Hasil luas lingkaran adalah: ", luasLingkaran)
+
+let kelilingLingkaran = 2 * pi * jariJariLingkaran
+console.log("Hasil keliling lingkaran adalah: ", kelilingLingkaran)
