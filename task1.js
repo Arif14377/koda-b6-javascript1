1d47f057c47345949b495a786c28afd177425820
Arif14377 <52228176+Arif14377@users.noreply.github.com>
2025-12-24 11:51:11 +0700

    Add files via upload

--- task1.js
+++ task1.js
@@ -5,8 +5,8 @@
 let jariJariLingkaran = 14
 const pi = 3.14
 
-let luasLingkaran = pi * jariJariLingkaran * jariJariLingkaran
+const luasLingkaran = pi * jariJariLingkaran * jariJariLingkaran
 console.log(luasLingkaran)
 
-let kelilingLingkaran = 2 * pi * jariJariLingkaran
+const kelilingLingkaran = 2 * pi * jariJariLingkaran
 console.log(kelilingLingkaran)
